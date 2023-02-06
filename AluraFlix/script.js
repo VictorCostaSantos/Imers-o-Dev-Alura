26145bd16f2ec074512231d91298e5a9a64e77d2
Victor Costa <victorcosta123vcs@gmail.com]>
2023-02-06 17:22:14 -0300

    Ultimas alterações

--- AluraFlix/script.js
+++ AluraFlix/script.js
@@ -1,68 +1,62 @@
-// Mundo HTML
-var campo = document.querySelector("#input-text")
-var titulo = document.getElementById("input-nome")
-var link = document.querySelector("#input-link")
+var tipoImagem = ['.jpg','.png','.bmp','.gif']
+var todosFilmes = ["django Livre","Matilda","Um sonho de liberdade","As branquelas"]
+var todosImagens = ["https://upload.wikimedia.org/wikipedia/pt/8/8b/Django_Unchained_Poster.jpg","https://br.web.img2.acsta.net/c_310_420/pictures/210/123/21012312_20130613132958221.jpg","https://br.web.img2.acsta.net/medias/nmedia/18/90/16/48/20083748.jpg","https://upload.wikimedia.org/wikipedia/pt/d/de/White_chicks.jpeg"]
+var todosTrailers = ["https://www.youtube.com/watch?v=tivv135aGbc","https://www.youtube.com/watch?v=QD12PO3cTug","https://www.youtube.com/watch?v=v1nZq1vfgSw","https://www.youtube.com/watch?v=z7fOQZWJyQA"]
+var elementoListaFilmes = document.getElementById("listaFilmes")
+var id = null
 
+mostrarFilme()
 
-var Valida = document.getElementById("texto");
-var mural = document.getElementById("mural")
-
-//efeitocarta
-
-var carta = document.getElementsByClassName("carta")
-var frente = document.getElementById("frente")
-var back = document.getElementById("back")
-
-// Fim mundo HTML
-
-var listaFilmes = [];
-var i = 0;
-
-while(i < listaFilmes.length)
-{
-        document.write("<img src=" + listaFilmes[i] + ">");       
-        i++       
+function adicionarFilme(){
+  var nomeFilme = document.getElementById('nome').value
+  var cartazFilme = document.getElementById('filme').value
+  var trailerFilme = document.getElementById('trailer').value
+  
+ if((testaImagem(cartazFilme))==1){
+    todosFilmes.push(nomeFilme)
+    todosImagens.push(cartazFilme)
+    todosTrailers.push(trailerFilme)
+    limparCampos()
+  } else{
+    alert('O link não é de uma imagem')
+    limparCampos()
+  }
+  mostrarFilme()
 }
- i = listaFilmes.length
 
-function Inserir(){
+function testaImagem(cartazFilme){
+  for(var i =0; i<tipoImagem.length; i++){
+    if(cartazFilme.endsWith(tipoImagem[i])){
+      return 1
+    }
+  }
+}
+
+function mostrarFilme(){
+  elementoListaFilmes.innerHTML = ''
+  var lista=''
+  for(var j=0; j<todosFilmes.length; j++){
+    lista += '<div class="listaFilmes" >'
+    lista += '<div class="itens"><div class="titulo"><h3>'+todosFilmes[j]+'</h3> </div>'
+    lista += '<a target=_blank href='+todosTrailers[j]+'>'
+    lista += '<button style="margin:-1% 0% 0% 13%" class="remove" onClick="removerFilme('+ j +')">X</button>'
+    lista += '<img class="imgs" src='+todosImagens[j]+'></a> </div>'
+    lista += '</div>'
     
-     if(listaFilmes.includes(campo.value)) {
-        alert("O filme já existe na lista");
-      }else if(campo.value.endsWith(".jpg") ||
-    campo.value.endsWith(".JPG") ||
-    campo.value.endsWith(".jpeg") ||
-    campo.value.endsWith(".JPEG") ||
-    campo.value.endsWith(".gif") ||
-    campo.value.endsWith(".GIF") ||
-    campo.value.endsWith(".png") ||
-    campo.value.endsWith(".PNG") ||
-    campo.value.endsWith(".svg") ||
-    campo.value.endsWith(".SVG") ||
-    campo.value.endsWith(".bmp") ||
-    campo.value.endsWith(".BMP") ||
-    campo.value.endsWith(".webp") ||
-    campo.value.endsWith(".WEBP") ){
-
-      listaFilmes.push(campo.value);
-        mural.innerHTML += `<div class="padrao">
-        <input type="button" id="deletar" value="Remover" onclick="deletar()"><a href="${link.value}"><img src="${listaFilmes[i]}"></a>
-        <h3>${titulo.value}</h3>      
-        </div>`;
-        i ++ 
-        
-      }
-      else{
-        alert("Não aceitamos esse formato")
-      }
-
-      campo.value = ""
-
-} 
-
-function deletar(){
-
+    elementoListaFilmes.innerHTML = lista
+  }
+}
+  
+function removerFilme(id)
+{
+  todosImagens.splice(id, 1)
+  todosTrailers.splice(id, 1)
+  todosFilmes.splice(id, 1)
+  mostrarFilme()
 }
 
-
-
+function limparCampos(){
+  document.getElementById('nome').value = null
+  document.getElementById('filme').value = null
+  document.getElementById('trailer').value = null
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Imers-o-Dev-Alura
+<div style="align-items: center;
+text-align: center;">
+ <img src="https://imersao.dev/assets/img/logo-imersao-dev6-desktop.1674580904.png" alt="">
+ <h5>Status do projeto: <img style="width: 20px;" src="https://gifs.eco.br/wp-content/uploads/2022/11/gifs-de-pronto-0.gif" alt=""></h5>
+</div>
